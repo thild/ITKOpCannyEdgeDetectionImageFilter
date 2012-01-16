94fab369cac800ea1bce183af8b6c92b1831dc16
Tony Alexander Hild <tony_hild@yahoo.com>
2012-01-16 09:19:12 -0200

    Using boost filesystem 2

--- src/ITKOpCannyEdgeDetectionImageFilter/main.cpp
+++ src/ITKOpCannyEdgeDetectionImageFilter/main.cpp
@@ -60,7 +60,7 @@
 #include <boost/foreach.hpp>
 #include <boost/lexical_cast.hpp>
 
-#define BOOST_FILESYSTEM_VERSION 2
+#define BOOST_FILESYSTEM_VERSION 3
 #define foreach BOOST_FOREACH
 
 namespace fs = boost::filesystem;
